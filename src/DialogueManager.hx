a9ccd8b2a867609639cabea7c655325472848470
Cody Claborn <cxsquared@gmail.com>
2025-06-27 18:02:54 -0400

    Most art done

--- src/DialogueManager.hx
+++ src/DialogueManager.hx
@@ -10,99 +10,96 @@ import hxyarn.compiler.Compiler;
 import hxyarn.compiler.CompilationJob;
 
 class DialogueManager {
-    public var dialogue:Dialogue;
-    var storage = new MemoryVariableStore();
-    var stringTable:Map<String, StringInfo>;
+	public var dialogue:Dialogue;
+
+	var storage = new MemoryVariableStore();
+	var stringTable:Map<String, StringInfo>;
 
 	public var lineHandlerCallback:LineHandler;
-    public var optionHandlerCallback:OptionsHandler;
+	public var optionHandlerCallback:OptionsHandler;
 
-    public function new() {
-        dialogue = new Dialogue(new MemoryVariableStore());
+	public function new() {
+		dialogue = new Dialogue(new MemoryVariableStore());
 
-        dialogue.library.registerFunction("success", 0,
-        function(values:Array<Value>) {
-            trace("success");
-            dialogue.resume();
-            return "Success +";
-        }, BuiltInTypes.string);
+		dialogue.library.registerFunction("success", 0, function(values:Array<Value>) {
+			trace("success");
+			dialogue.resume();
+			return "Success +";
+		}, BuiltInTypes.string);
 
-        dialogue.logDebugMessage = this.logDebugMessage;
-        dialogue.logErrorMessage = this.logErrorMessage;
-        dialogue.lineHandler = this.lineHandler;
-        dialogue.optionsHandler = this.optionsHandler;
-        dialogue.commandHandler = this.commandHandler;
-        dialogue.nodeCompleteHandler = this.nodeCompleteHandler;
-        dialogue.nodeStartHandler = this.nodeStartHandler;
-        dialogue.dialogueCompleteHandler = this.dialogueCompleteHandler;
+		dialogue.logDebugMessage = this.logDebugMessage;
+		dialogue.logErrorMessage = this.logErrorMessage;
+		dialogue.lineHandler = this.lineHandler;
+		dialogue.optionsHandler = this.optionsHandler;
+		dialogue.commandHandler = this.commandHandler;
+		dialogue.nodeCompleteHandler = this.nodeCompleteHandler;
+		dialogue.nodeStartHandler = this.nodeStartHandler;
+		dialogue.dialogueCompleteHandler = this.dialogueCompleteHandler;
 
-        this.loadFromString([hxd.Res.text.start.entry.getText()], [hxd.Res.text.start.name]);
-    }
+		this.loadFromString([hxd.Res.text.start.entry.getText()], [hxd.Res.text.start.name]);
+	}
 
-    public function loadFromString(text:Array<String>, fileNames:Array<String>) {
-        var job = CompilationJob.createFromStrings(text, fileNames, dialogue.library);
-        var compiler = Compiler.compile(job);
-        stringTable = compiler.stringTable;
+	public function loadFromString(text:Array<String>, fileNames:Array<String>) {
+		var job = CompilationJob.createFromStrings(text, fileNames, dialogue.library);
+		var compiler = Compiler.compile(job);
+		stringTable = compiler.stringTable;
 
-        dialogue.addProgram(compiler.program);
-    }
+		dialogue.addProgram(compiler.program);
+	}
 
-    public function runNode(nodeName:String) {
-        dialogue.setNode(nodeName);
-        dialogue.resume();
-    }
+	public function runNode(nodeName:String) {
+		dialogue.setNode(nodeName);
+		dialogue.resume();
+	}
 
-    public function unload() {
-        dialogue.unloadAll();
-    }
+	public function unload() {
+		dialogue.unloadAll();
+	}
 
-    public function resume() {
-        dialogue.resume();
-    }
+	public function resume() {
+		dialogue.resume();
+	}
 
-    public function logDebugMessage(message:String):Void {
-    }
+	public function logDebugMessage(message:String):Void {}
 
-    public function logErrorMessage(message:String):Void {
-    }
+	public function logErrorMessage(message:String):Void {}
 
-    public function lineHandler(line:Line):HandlerExecutionType {
-        if (this.lineHandlerCallback != null) {
-            this.lineHandlerCallback(line);
-        }
+	public function lineHandler(line:Line):HandlerExecutionType {
+		if (this.lineHandlerCallback != null) {
+			this.lineHandlerCallback(line);
+		}
 
-        return HandlerExecutionType.ContinueExecution;
-    }
+		return HandlerExecutionType.ContinueExecution;
+	}
 
-    public function optionsHandler(options:OptionSet) {
-        if (this.optionHandlerCallback != null) {
-            this.optionHandlerCallback(options);
-        }
-    }
+	public function optionsHandler(options:OptionSet) {
+		if (this.optionHandlerCallback != null) {
+			this.optionHandlerCallback(options);
+		}
+	}
 
-    public function getComposedTextForLine(line:Line):String {
-        var substitutedText = Dialogue.expandSubstitutions(stringTable[line.id].text, line.substitutions);
+	public function getComposedTextForLine(line:Line):String {
+		var substitutedText = Dialogue.expandSubstitutions(stringTable[line.id].text, line.substitutions);
 
-        var markup = dialogue.parseMarkup(substitutedText);
+		var markup = dialogue.parseMarkup(substitutedText);
 
-        return markup.text;
-    }
+		return markup.text;
+	}
 
-    public function commandHandler(command:Command) {
-        trace(command.text + " command");
-        dialogue.resume();
-    }
+	public function commandHandler(command:Command) {
+		trace(command.text + " command");
+		dialogue.resume();
+	}
 
-    public function nodeCompleteHandler(nodeName:String) {
-        trace(nodeName + " completed");
-    }
+	public function nodeCompleteHandler(nodeName:String) {
+		trace(nodeName + " completed");
+	}
 
-    public function nodeStartHandler(nodeName:String) {
-        trace(nodeName + " started");
+	public function nodeStartHandler(nodeName:String) {
+		trace(nodeName + " started");
+	}
 
-    }
-
-    public function dialogueCompleteHandler() {
-        trace("Dialogue Completed");
-    }
+	public function dialogueCompleteHandler() {
+		trace("Dialogue Completed");
+	}
 }
--- src/Main.hx
+++ src/Main.hx
@@ -1,83 +1,155 @@
+import h2d.Bitmap;
+import h2d.Graphics;
+import h2d.Tile;
+import h2d.Flow.FlowAlign;
 import hxyarn.dialogue.OptionSet;
 import hxd.Key;
 import h2d.Text.Align;
+import h2d.HtmlText;
 import hxyarn.dialogue.Line;
 import hxyarn.dialogue.Dialogue.HandlerExecutionType;
 
 class Main extends hxd.App {
-    var dialogue:DialogueManager;
-    var options:OptionSet = null;
+	var dialogue:DialogueManager;
+	var options:OptionSet = null;
+	var backgrounds:Array<Tile>;
+	var bgIndex = 8;
+	var bg:Bitmap;
 
-    override function init() {
-        s2d.scaleMode = ScaleMode.LetterBox(800, 600);
-        s2d.setScale(2);
+	var onTitle = true;
+	var textBox:Bitmap;
 
-        // Heaps resources
+	override function init() {
+		// Heaps resources
 		#if (hl && debug)
 		hxd.Res.initLocal();
 		#else
 		hxd.Res.initEmbed();
 		#end
 
-        var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-        tf.textAlign = Align.Left; 
-        tf.setScale(1);
-        tf.maxWidth = 600;
-        tf.tileWrap = true;
-        tf.text = "";
+		var width = 800;
+		var height = 600;
 
-        dialogue = new DialogueManager();
-        dialogue.lineHandlerCallback = function (line:Line):HandlerExecutionType {
-            var text = dialogue.getComposedTextForLine(line);
+		s2d.scaleMode = ScaleMode.LetterBox(width, height);
 
-            // Show text
-            tf.text = text; 
+		var bgWidth = 256;
+		var bgHeight = 224;
 
-            return HandlerExecutionType.ContinueExecution;
-        }
-        dialogue.optionHandlerCallback = function (options:OptionSet) {
-            this.options = options;
-            tf.text = "";
-            var optionText = new Array<String>();
+		var backgroundTile = hxd.Res.images.scenes.toTile();
+		backgrounds = new Array<Tile>();
 
-            for (option in options.options) {
-                var text = dialogue.getComposedTextForLine(option.line);
-                optionText.push(text);
-                if (tf.text == "") {
-                    tf.text = text;
-                } else {
-                    tf.text = tf.text + "\n" + text;
-                }
-            }
-        }
-        dialogue.runNode("WakeUp");
-    }
+		for (y in 0...3) {
+			for (x in 0...3) {
+				var t = backgroundTile.sub(x * bgWidth, y * bgHeight, bgWidth, bgHeight);
+				if (t != null) {
+					backgrounds.push(t);
+				}
+			}
+		}
 
-    override function update(dt:Float) {
-        super.update(dt);
+		bg = new Bitmap(backgrounds[bgIndex], s2d);
+		bg.setScale(height / bgHeight);
+		bg.x = (width / 2) - (bg.getSize().width / 2);
 
-        if (options == null && Key.isPressed(Key.SPACE)) {
-            dialogue.resume();
-        }
+		textBox = new h2d.Bitmap(Tile.fromColor(0x4A0E99, width - 32, Std.int(height / 2) - 32, .7), s2d);
+		textBox.width = width - 32;
+		textBox.height = Std.int(height / 2) - 32;
+		textBox.x = 16;
+		textBox.y = (height / 2) + 16;
 
-        if (options != null) {
-            if (Key.isPressed(Key.NUMBER_1)) {
-                dialogue.dialogue.setSelectedOption(0);
-                options = null;
-                dialogue.resume();
-            } else if (options.options.length > 1 && Key.isPressed(Key.NUMBER_2)) {
-                dialogue.dialogue.setSelectedOption(1);
-                options = null;
-                dialogue.resume();
-            } else if (options.options.length > 2 && Key.isPressed(Key.NUMBER_3)) {
-                dialogue.dialogue.setSelectedOption(2);
-                options = null;
-                dialogue.resume();
-            }
-        }
-    }
+		var flow = new h2d.Flow(textBox);
+		flow.borderWidth = 8;
+		flow.borderHeight = 8;
+		flow.padding = 16;
+		flow.horizontalAlign = FlowAlign.Left;
+		flow.verticalAlign = FlowAlign.Top;
+		flow.maxWidth = Std.int(textBox.width) - 8;
+		flow.maxHeight = Std.int(textBox.height) - 8;
 
-    static function main() {
-        new Main();
-    }
+		var tf = new HtmlText(hxd.res.DefaultFont.get(), flow);
+
+		tf.textAlign = Align.Left;
+		tf.setScale(2);
+		tf.text = "";
+		tf.maxWidth = flow.maxWidth - 32;
+
+		textBox.visible = false;
+
+		dialogue = new DialogueManager();
+		dialogue.lineHandlerCallback = function(line:Line):HandlerExecutionType {
+			var text = dialogue.getComposedTextForLine(line);
+
+			// Show text
+			tf.text = text;
+			trace(tf.textWidth);
+
+			return HandlerExecutionType.ContinueExecution;
+		}
+		dialogue.optionHandlerCallback = function(options:OptionSet) {
+			this.options = options;
+			tf.text = "";
+			var optionText = new Array<String>();
+
+			for (option in options.options) {
+				var text = dialogue.getComposedTextForLine(option.line);
+				optionText.push(text);
+				if (tf.text == "") {
+					tf.text = '(${option.id + 1}) $text';
+				} else {
+					tf.text = tf.text + '<br/>(${option.id + 1}) $text';
+				}
+			}
+		}
+
+		dialogue.dialogue.nodeCompleteHandler = function(nodeName:String) {
+			trace(nodeName + " completed");
+			bgIndex++;
+			if (bgIndex < backgrounds.length) {
+				bg.tile = backgrounds[bgIndex];
+			} else {
+				bg.visible = false;
+			}
+		}
+	}
+
+	override function update(dt:Float) {
+		super.update(dt);
+
+		if (onTitle && Key.isPressed(Key.SPACE)) {
+			onTitle = false;
+			dialogue.runNode("WakeUp");
+			textBox.visible = true;
+			bg.tile = backgrounds[0];
+			bgIndex = 0;
+			onTitle = false;
+		}
+
+		if (onTitle) {
+			return;
+		}
+
+		if (options == null && Key.isPressed(Key.SPACE)) {
+			dialogue.resume();
+		}
+
+		if (options != null) {
+			if (Key.isPressed(Key.NUMBER_1)) {
+				dialogue.dialogue.setSelectedOption(0);
+				options = null;
+				dialogue.resume();
+			} else if (options.options.length > 1 && Key.isPressed(Key.NUMBER_2)) {
+				dialogue.dialogue.setSelectedOption(1);
+				options = null;
+				dialogue.resume();
+			} else if (options.options.length > 2 && Key.isPressed(Key.NUMBER_3)) {
+				dialogue.dialogue.setSelectedOption(2);
+				options = null;
+				dialogue.resume();
+			}
+		}
+	}
+
+	static function main() {
+		new Main();
+	}
 }
