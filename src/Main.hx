6d87f62b30c3de7fd94fe0d7e397a453799fa8a4
Cxsquared <cxsquared@gmail.com>
2025-06-27 20:42:19 -0400

    Better text box

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,9 @@
+import hxd.res.Font;
+import h2d.Text;
+import hxd.Math;
+import hxd.Timer;
+import h3d.Engine;
 import h2d.Bitmap;
-import h2d.Graphics;
 import h2d.Tile;
 import h2d.Flow.FlowAlign;
 import hxyarn.dialogue.OptionSet;
@@ -15,6 +19,14 @@ class Main extends hxd.App {
 	var backgrounds:Array<Tile>;
 	var bgIndex = 8;
 	var bg:Bitmap;
+	var currentLine = null;
+	var charactersPerSecond = 30;
+	var progressLength:Int;
+	var progress:Int = 0;
+	var textElapsed:Float = 0;
+	var text:HtmlText;
+	var continueText:Text;
+	var firstFrame = true;
 
 	var onTitle = true;
 	var textBox:Bitmap;
@@ -26,6 +38,9 @@ class Main extends hxd.App {
 		#else
 		hxd.Res.initEmbed();
 		#end
+
+		var ctx = Engine.getCurrent();
+		ctx.backgroundColor = 0x181425;
 
 		var width = 800;
 		var height = 600;
@@ -51,11 +66,11 @@ class Main extends hxd.App {
 		bg.setScale(height / bgHeight);
 		bg.x = (width / 2) - (bg.getSize().width / 2);
 
-		textBox = new h2d.Bitmap(Tile.fromColor(0x4A0E99, width - 32, Std.int(height / 2) - 32, .7), s2d);
+		textBox = new h2d.Bitmap(Tile.fromColor(0x000000, width - 32, Std.int(height / 2.5) - 32, .6), s2d);
 		textBox.width = width - 32;
-		textBox.height = Std.int(height / 2) - 32;
+		textBox.height = Std.int(height / 2.5) - 32;
 		textBox.x = 16;
-		textBox.y = (height / 2) + 16;
+		textBox.y = height - (height / 2.5) + 16;
 
 		var flow = new h2d.Flow(textBox);
 		flow.borderWidth = 8;
@@ -66,43 +81,58 @@ class Main extends hxd.App {
 		flow.maxWidth = Std.int(textBox.width) - 8;
 		flow.maxHeight = Std.int(textBox.height) - 8;
 
-		var tf = new HtmlText(hxd.res.DefaultFont.get(), flow);
+		text = new HtmlText(hxd.Res.fonts.lekton.toFont(), flow);
 
-		tf.textAlign = Align.Left;
-		tf.setScale(2);
-		tf.text = "";
-		tf.maxWidth = flow.maxWidth - 32;
+		text.textAlign = Align.Left;
+		text.setScale(1);
+		text.text = "";
+		text.maxWidth = flow.maxWidth - 32;
+
+		continueText = new Text(hxd.Res.fonts.lekton.toFont(), textBox);
+		continueText.setScale(.8);
+		continueText.text = "Press space to continue";
+		continueText.x = textBox.width - 8 - (continueText.textWidth * .8);
+		continueText.y = textBox.height - (continueText.textHeight * .8);
+		continueText.visible = false;
 
 		textBox.visible = false;
 
 		dialogue = new DialogueManager();
 		dialogue.lineHandlerCallback = function(line:Line):HandlerExecutionType {
-			var text = dialogue.getComposedTextForLine(line);
+			var textString = dialogue.getComposedTextForLine(line);
+			currentLine = textString;
+			progress = 0;
+			progressLength = textString.length;
 
-			// Show text
-			tf.text = text;
-			trace(tf.textWidth);
+			text.text = "";
+			firstFrame = true;
+			textElapsed = 0;
 
 			return HandlerExecutionType.ContinueExecution;
 		}
 		dialogue.optionHandlerCallback = function(options:OptionSet) {
+			continueText.visible = false;
 			this.options = options;
-			tf.text = "";
+			text.text = "";
 			var optionText = new Array<String>();
 
 			for (option in options.options) {
-				var text = dialogue.getComposedTextForLine(option.line);
-				optionText.push(text);
-				if (tf.text == "") {
-					tf.text = '(${option.id + 1}) $text';
+				var textString = dialogue.getComposedTextForLine(option.line);
+				optionText.push(textString);
+				if (text.text == "") {
+					text.text = '(${option.id + 1}) $textString';
 				} else {
-					tf.text = tf.text + '<br/>(${option.id + 1}) $text';
+					text.text = text.text + '<br/>(${option.id + 1}) $textString';
 				}
 			}
 		}
 
 		dialogue.dialogue.nodeCompleteHandler = function(nodeName:String) {
-			trace(nodeName + " completed");
+			if (nodeName == "Promotion") {
+				bg.visible = false;
+				return;
+			}
+
 			bgIndex++;
 			if (bgIndex < backgrounds.length) {
 				bg.tile = backgrounds[bgIndex];
@@ -112,24 +142,50 @@ class Main extends hxd.App {
 		}
 	}
 
+	function startGame() {
+		onTitle = false;
+		dialogue.runNode("WakeUp");
+		textBox.visible = true;
+		bg.tile = backgrounds[0];
+		bgIndex = 0;
+		onTitle = false;
+	}
+
 	override function update(dt:Float) {
 		super.update(dt);
 
 		if (onTitle && Key.isPressed(Key.SPACE)) {
-			onTitle = false;
-			dialogue.runNode("WakeUp");
-			textBox.visible = true;
-			bg.tile = backgrounds[0];
-			bgIndex = 0;
-			onTitle = false;
+			startGame();
 		}
 
 		if (onTitle) {
 			return;
 		}
 
-		if (options == null && Key.isPressed(Key.SPACE)) {
-			dialogue.resume();
+		if (currentLine != null && progress < progressLength) {
+			textElapsed += Timer.elapsedTime;
+			progress = Math.floor(textElapsed * charactersPerSecond);
+			text.text = text.getTextProgress(currentLine, progress);
+
+			if (progress >= progressLength) {
+				currentLine = null;
+				continueText.visible = true;
+			}
+		}
+
+		if (!firstFrame && options == null && Key.isPressed(Key.SPACE)) {
+			if (progress < progressLength) {
+				progress = progressLength;
+				text.text = currentLine;
+				continueText.visible = true;
+			} else {
+				dialogue.resume();
+				continueText.visible = false;
+			}
+		}
+
+		if (firstFrame) {
+			firstFrame = false;
 		}
 
 		if (options != null) {
